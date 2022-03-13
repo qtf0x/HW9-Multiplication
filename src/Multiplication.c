fb8cb79ab2ea526e159c8038a69e807ee2f3d35c
Vincent Marias <vmarias@mines.edu>
2022-03-13 16:49:32 -0600

    fixed typo in header comments

--- src/Multiplication.c
+++ src/Multiplication.c
@@ -1,7 +1,7 @@
 /**
  * @file Multiplication.c
  * @author Vincent Marias (vmarias@mines.edu)
- * @brief Implementation of the optimized hardware multiplication alogrithm.
+ * @brief Implementation of the optimized hardware multiplication algorithm.
  * @date 2022-03-13
  */
 
--- src/Multiplication.s
+++ src/Multiplication.s
@@ -1,6 +1,6 @@
 /**
  * @file Multiplication.s
  * @author Vincent Marias (vmarias@mines.edu)
- * @brief Implementation of the optimized hardware multiplication alogrithm.
+ * @brief Implementation of the optimized hardware multiplication algorithm.
  * @date 2022-03-13
  */
